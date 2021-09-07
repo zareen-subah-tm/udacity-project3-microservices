34a57c46ad36c73c1011aa2d26a1c0db74f3a834
Zareen Subah (TMNA) <zareen.subah@toyota.com>
2021-09-07 16:41:15 -0500

    updated ignore file

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=zareen
+export POSTGRES_PASSWORD=Subah1108!
+export POSTGRES_HOST=cdr.chfl50ftolyp.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=cdr
+export AWS_BUCKET=arn:aws:s3:::udagram-subah-micro
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
