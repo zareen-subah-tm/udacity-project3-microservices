518ba470c90a58f064af2f8604a29da798fbec94
Zareen Subah (TMNA) <zareen.subah@toyota.com>
2021-09-23 15:57:02 -0500

    Update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1d845afc90854bf990b73aaa4394118-199410495.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
